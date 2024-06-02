43462ed12f660a269568b79e62c2736e85521216
NicoOnIce <jackdongle104@gmail.com>
2024-06-02 21:05:30 +0000

    Add files via upload

--- autoheal.js
+++ autoheal.js
@@ -5,21 +5,21 @@ author: Nico
 
 import { breeze, Timer, mc, C09PacketHeldItemChange, playerController } from '../breeze_defs';
 
-const timer = new Timer()
-let healed = false
+const timer = new Timer();
 
 breeze.registerModule('heal', 'automaticly heal when you are low', {
 
+    healed: false,
     delay: new DoubleSetting('Health', 'The point at which auto heal, kicks in', 1, 0.5, 20),
     slot: new DoubleSetting('Hotbar slot', 'Where in your hotbar the healing item will be', 1, 0, 10),
-    bypass: new BooleanSetting('bypass', 'Should the module bypass watchdog', false),
+    bypass: new BooleanSetting('Bypass', 'If the module should use a watch dog bypassing method or not', false),
 
     tick: function(event) {
         player = mc.getPlayer()
         health = player.getHealth()
 
         if (health < this.delay.getValue()) {
-            if (!healed) {
+            if (!this.healed) {
                 breeze.postNotification('Auto heal', 'You reached your health thresh hold, now healing...')
                 breeze.sendPacket(new C09PacketHeldItemChange(this.slot.getValue()-1), this.bypass.getValue())
                 try {playerController.sendUseItem()} catch (err) {player.swingItem()}
@@ -27,7 +27,7 @@ breeze.registerModule('heal', 'automaticly heal when you are low', {
                 breeze.postNotification('Auto heal', 'Finished healing')
                 timer.reset()
             } else if (timer.hasPassed(2*1000)) {
-                healed = false
+                this.healed = false
             }
         }
     }
